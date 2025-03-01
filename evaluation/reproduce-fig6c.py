22d8acb6385fd362faa2433b6ec6e03d828f2b9d
Xuanlin Jiang <xljiang@stu.pku.edu.cn>
2025-03-01 03:06:38 +0000

    Complete fig6c data points

--- evaluation/reproduce-fig6c.py
+++ evaluation/reproduce-fig6c.py
@@ -14,7 +14,21 @@ with open(f"{cur_dir}/configs/config-t4-7b.json", "r") as f:
 async def one_round(server_name: str):
     start_server(server_name, config)
     try:
-        await run_test(*prepare_real_test("osc", config, server_name), 0.5)
+        # Change the rate argument (in reqs/s) to other values to see more results
+        # Feel free to comment out some of the following lines to reduce running time
+        if server_name == "ours":
+            await run_test(*prepare_real_test("osc", config, server_name), rate=0.5)
+            await run_test(*prepare_real_test("osc", config, server_name), rate=1.5)
+            await run_test(*prepare_real_test("osc", config, server_name), rate=2.5)
+            await run_test(*prepare_real_test("osc", config, server_name), rate=3.1)
+            await run_test(*prepare_real_test("osc", config, server_name), rate=3.5)
+            await run_test(*prepare_real_test("osc", config, server_name), rate=3.7)
+            await run_test(*prepare_real_test("osc", config, server_name), rate=3.9)
+        if server_name == "vllm":
+            await run_test(*prepare_real_test("osc", config, server_name), rate=0.2)
+            await run_test(*prepare_real_test("osc", config, server_name), rate=0.4)
+            await run_test(*prepare_real_test("osc", config, server_name), rate=0.5)
+            await run_test(*prepare_real_test("osc", config, server_name), rate=0.6)
     finally:
         stop_server()
     await asyncio.sleep(5)
